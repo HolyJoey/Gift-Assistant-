4c1a2b7fd6dec5ec3508c12953e3a8c20e2d8f8a
Holy <74293660+HolyJoey@users.noreply.github.com>
2023-12-20 17:17:44 +0100

    Update ChatHelpGifting.lua
    Added full customization

--- ChatHelpGifting.lua
+++ ChatHelpGifting.lua
@@ -1,35 +1,71 @@
 --Holyjoey.
---1.0
+--2.0
 
 util.require_natives('2944b')
 util.keep_running()
 
--- Define a variable to store the state of the help gifting feature
+local settings_list = menu.list(menu.my_root(), "Settings", {}, "", function() end)
+local customize_list = menu.list(settings_list, "Customize", {}, "", function() end)
+local disable_list = menu.list(settings_list, "Disables", {}, "", function() end)
+
 local automsg = false
 
--- Function to toggle the help gifting feature
-menu.toggle(menu.my_root(), "Help Gifting", { "helpgifting" }, "Enables a message to give help on gifting cars. It gets triggered by reading gift in chat.", function(on)
+menu.toggle(menu.my_root(), "Auto Chat", { "helpgifting" }, "Enables a message to give help on gifting cars. It gets triggered by reading gift in chat.", function(on)
     automsg = on
 end)
 
--- Function to handle incoming chat messages
+menu.toggle(disable_list, "Disable message introduction", { "intromessage" }, "Disables a message introducting the guide.", function(cancer)
+    intromsg = cancer
+end)
+
+menu.toggle(disable_list, "Disable message model names", { "modelmessage" }, "Disables a message telling people to use model names instead of full car names.", function(aids)
+    modelmsg = aids
+end)
+
+local intromessage = "> Guide on gifting cars:"
+menu.text_input(customize_list, "Change Intro Message", { "ChangeIntro" }, "", function(fuck)
+    intromessage = "> " .. fuck
+end)
+
+local guidemessage = "> 0: Fill up a garage completely (free cars, street cars, be creative). 1: Go to garage, drive any car outside and back inside. 2: Walk outside. 3: Spawn car with !carname e.g. !t20. 4: Get in car and !gift username. 5: Drive car in garage."
+menu.text_input(customize_list, "Change Guide Message", { "ChangeGuide" }, "", function(fuck2)
+    guidemessage = "> " .. fuck2
+end)
+
+local modelmessage = "> Dont use full car names but use the object names. Look on website gtahash.ru/cars for it or ask me."
+menu.text_input(customize_list, "Change Model Message", { "ChangeModel" }, "", function(fuck3)
+    modelmessage = "> " .. fuck3
+end)
+
+local triggerword = "gift"
+menu.text_input(customize_list, "Change Chat Triggerword", { "ChangeTrigger" }, "Change the word that triggers the message. Default is one is gift", function(fuck4)
+    triggerword = fuck4
+end)
+
 chat.on_message(function(pid, unused, content, tc)
-    -- Check if the help gifting feature is enabled
     if automsg then
-        -- Convert the message content to lowercase for case-insensitive matching
         local lowerContent = content:lower()
 
-        -- Check if the message contains the word 'gift' and does not start with '>' or '!'
-        if lowerContent:find('gift') and not lowerContent:find('> ') and not lowerContent:find('!') then
-            -- Send a help message about gifting cars
-            chat.send_message('> Guide on gifting cars:', tc, true, true)
-            chat.send_message('> 0: Fill up a garage completely (free cars, street cars, be creative). 1: Go to garage, drive any car outside and back inside. 2: Walk outside. 3: Spawn car with "!carname" e.g. "!t20". 4: Get in car and "!gift username". 5: Drive car in garage.', tc, true, true)
-            chat.send_message('> Dont use full car names but use the object names. Look on website gtahash.ru/cars for it or ask me.', tc, true, true)        end
+        if lowerContent:find(triggerword) then
+            if not lowerContent:find('> ') and not lowerContent:find('!') then
+                if not intromsg then
+                    chat.send_message(intromessage, tc, true, true)
+                end
+                chat.send_message(guidemessage, tc, true, true)
+                if not modelmsg then
+                    chat.send_message(modelmessage, tc, true, true)
+                end
+            end
+        end
     end
 end)
 
 menu.action(menu.my_root(), "Manually push message", {}, "Put guide in chat", function()
-    chat.send_message('> Guide on gifting cars:', tc, true, true)
-    chat.send_message('> 0: Fill up a garage completely (free cars, street cars, be creative). 1: Go to garage, drive any car outside and back inside. 2: Walk outside. 3: Spawn car with "!carname" e.g. "!t20". 4: Get in car and "!gift username". 5: Drive car in garage.', tc, true, true)
-    chat.send_message('> Dont use full car names but use the object names. Look on website gtahash.ru/cars for it or ask me.', tc, true, true)
+    if not intromsg then
+        chat.send_message(intromessage, tc, true, true)
+    end
+    chat.send_message(guidemessage, tc, true, true)
+    if not modelmsg then
+        chat.send_message(modelmessage, tc, true, true)
+    end
 end)
